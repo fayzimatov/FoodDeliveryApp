c556da387db784d7be50b3851b685808e9597625
UmidKhan <ufayzimatov4@gmail.com>
2024-10-04 10:49:34 +0500

    Created base project, removed storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Umidjon Fayzimatov on 04/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+
+
+}
+
