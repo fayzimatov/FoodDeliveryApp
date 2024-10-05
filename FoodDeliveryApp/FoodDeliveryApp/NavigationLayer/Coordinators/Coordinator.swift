39d6f28815df0ef141a64c2370f2b91419ce38a3
UmidKhan <ufayzimatov4@gmail.com>
2024-10-05 10:47:07 +0500

    Added coordinators protocol

--- FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/Coordinator.swift
+++ FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/Coordinator.swift
@@ -5,4 +5,43 @@
 //  Created by Umidjon Fayzimatov on 05/10/24.
 //
 
-import Foundation
+import UIKit
+
+
+enum CoordinatorType {
+    case app
+    case onboarding
+    case home
+    case order
+    case list
+    case profile
+    
+}
+
+
+protocol CoordinatorProtocol: AnyObject {
+    
+    var childCoordinators: [CoordinatorProtocol] { get set }
+    var type: CoordinatorType { get }
+    var navigationController: UINavigationController? { get set }
+    var finishDelegate: CoordinatorFinishDelagete? { get set }
+    
+    func start()
+    func finish()
+    
+}
+
+
+extension CoordinatorProtocol {
+    func addChildCoordinator(_ childCoordinator: CoordinatorProtocol) {
+        childCoordinators.append(childCoordinator)
+    }
+    
+    func removeChildCoordinator(_ childCoordinator: CoordinatorProtocol) {
+        childCoordinators = childCoordinators.filter { $0 !== childCoordinator }
+    }
+}
+protocol CoordinatorFinishDelagete: AnyObject {
+    func coordinatorDidFinish(childCoordinator: CoordinatorProtocol)
+    
+}
